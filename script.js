81c011230dba09d33211dc8e3b3025cf97cccd35
anuthapaliy <113211321+anuthapaliy@users.noreply.github.com>
2022-09-30 15:03:48 +0100

    Update script.js
    More Project-Add Comment

--- script.js
+++ script.js
@@ -111,3 +111,14 @@ document.getElementById('previous').addEventListener('click', (event) => {
   element_carousel3.setAttribute("src", images.slice(-1)[0]);
 
 });
+
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
+
